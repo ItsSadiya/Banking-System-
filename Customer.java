6af72e1204fbc79fa2670775a1e678bb6a7f1710
Sadiya Sultana <112492609+ItsSadiya@users.noreply.github.com>
2023-07-11 01:20:14 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,14 @@
+class Customer
+{
+    String name;
+    String EpicNo;
+    long AdharNo;
+    String Address;
+    int PIN;
+    String AC_No;
+    String Fname;
+    int AC_Type;
+    double OPBlnce;
+    long MobNo;
+    String PAN;
+}
